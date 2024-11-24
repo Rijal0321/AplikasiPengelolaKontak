bf97410196ab673266b78494e269544caffea15b
Muhammad Rijal Aditya <136682708+Rijal0321@users.noreply.github.com>
2024-11-24 15:16:31 +0800

    Menambahkan java class untuk koneksi database

---
+++ src/KoneksiDatabase.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KoneksiDatabase {
+        public static Connection getKoneksi() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/pengelola_kontak"; // Sesuaikan nama database
+            String pengguna = "root"; // Sesuaikan username MySQL
+            String sandi = ""; // Sesuaikan password MySQL
+            return DriverManager.getConnection(url, pengguna, sandi);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
